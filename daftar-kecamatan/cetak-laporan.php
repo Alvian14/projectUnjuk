cc4cea77c27c87c35ccf68f876d4947a59d14f94
Alvian14 <cidukgaming03@gmail.com>
2023-11-20 10:00:11 +0700

    update

--- daftar-kecamatan/cetak-laporan.php
+++ daftar-kecamatan/cetak-laporan.php
@@ -32,6 +32,7 @@
         echo "Laporan UMKM - Kecamatan " . $kecamatan;
         ?>
   </title>
+  <link rel="website icon" href="../tem-login/images/logoUnjuk.png">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
